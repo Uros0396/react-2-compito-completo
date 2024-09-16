5982b4769823ec0061c23425650cc3cc9a2eb5b5
uros milenkovic <urosm4471@gmail.com>
2024-09-16 18:16:13 +0200

    modifica

--- demo1/src/components/MyNav.jsx
+++ demo1/src/components/MyNav.jsx
@@ -4,7 +4,7 @@ import {Container, Row, Col } from "react-bootstrap"
 const MyNav = () => {
     return (
      <nav>
-        <Container fluid className="fixed-top">
+        <Container fluid className="fixed-top mt-5">
             <Row>
                 <Col lg={12} className="d-flex justify-content-around align-items-center bg-dark">
                 <p className="text-light">Home</p>
